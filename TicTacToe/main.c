0a9a6493b6e34d94881ded69fbb52fb16a3ac2a6
Alexandru Chiriac <alexandruchi203@icloud.com>
2022-02-17 22:15:33 +0200

    Initial Commit

---
+++ TicTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Alexandru Chiriac on 17.02.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
